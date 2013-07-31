ed3521e2fb8560ef0b357192959002b651d5fa15
gscharf <gregscharf@gmail.com>
2013-07-30 22:14:36 -0700

    first commit

---
+++ js/project_manager.js
@@ -0,0 +1,10 @@
+var project_mgr = project_mgr || {};
+var ENTER_KEY = 13;
+
+$(function() {
+
+	// Kick things off by creating the **App**.
+	project_mgr.LoginView = new project_mgr.LoginView();
+	project_mgr.EntryEditorButton = new project_mgr.EntryEditorButton();
+
+});
